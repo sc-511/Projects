8ec7ea0c61fe8cc0769f1773b97861b37046e22b
Shane Craig <shanecraig99@live.com>
2020-11-21 12:28:17 -0500

    Updated the look and code within the submitted program.
    Javascript was entirely redone on both programs and still have to be tweaked and same with the css to make it appeal alot cleaner for the user.

--- Fortune Cookie - JavaScript/FortuneCookie.js
+++ Fortune Cookie - JavaScript/FortuneCookie.js
@@ -1,28 +1,37 @@
-let userName = 'Shane';
-
-console.log("Welcome to the fortune cookie!");
-console.log("Let the fortune cookie choose your destiny");
-
-let fortuneCookieValue = 5;
-let questionToAsk = "What will my future look like?...";
-switch(Math.random() * fortuneCookieValue){
-    case 0:
-        console.log("It is uncertain....");
-        break;
-    case 1:
-        console.log("You will be very rich..");
-        break;
-    case 2:
-        console.log("You will live a life of unfortuante events..");
-        break;
-    case 3:
-        console.log("You will live a life of happiness...");
-        break;
-    case 4:
-        console.log("A special person will be visiting soon with great news..");
-        break;
-    case 5:
-        console.log("You will be finding yourself with a great amount of success...");
-        break;
+function fortuneGenerator(){
+    let fortuneCookieValue = 6;
+    const fortuneValue = document.getElementById("fortuneOutput");
+    const userName = document.getElementById("userInput").value;
+    let randomValue = Math.random() * fortuneCookieValue;
+    switch(randomValue){
+        case 0:
+            document.getElementById("fortuneOutput").innerHTML= userName + ", " + "It is uncertain....";
+            console.log("inside case zero");
+             break;
+        case 1:
+            fortuneValue.innerHTML = userName + ", " + "You will be very rich..";
+            break;
+        case 2:
+            fortuneValue.innerHTML = userName + ", " + "You will live a life of unfortuante events..";
+            break;
+        case 3:
+            fortuneValue.innerHTML = userName + ", " +"You will live a life of happiness...";
+            break;
+        case 4:
+            fortuneValue.innerHTML = userName + ", " + "A special person will be visiting soon with great news..";
+            break;
+        case 5:
+            fortuneValue.innerHTML = userName + ", " + "You will be finding yourself with a great amount of success...";
+            break;
+        default:
+             console.log("an error has occured");
+             break;
+        }
 }
-console.log("Thank you for playing!");
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const cookieButton = document.getElementById('cookieButton');
+    cookieButton.addEventListener('click', (event) =>{
+        fortuneGenerator();
+    });
+});
--- Temp Converter - JavaScript/Tempature.js
+++ Temp Converter - JavaScript/Tempature.js
@@ -2,17 +2,65 @@
 // let userValue = 45;
 // const celciusToFarenheit = (userValue / 5) * (9 + 32);
 // const farenheitToCelcius = (userValue - 32) * (5 / 9);
+// var degreeValue = prompt("Please enter your temperature you wish to convert");
+// degreeValue = parseInt(degreeValue);
+// var userTypeInput = prompt("What degree is this?");
+//if (Number.isInteger(degreeValue) && userTypeInput.startsWith('f')){
+//      console.log("Here is your conversion: " + fahrenheitToCelsius + "C");
+// } else if (Number.isInteger(degreeValue) && userTypeInput.startsWith('c')){
+//      console.log("Here is your conversion: " + celsiusToFahrenheit + "F" );
+// } else {
+//      console.log("Invalid entry please try again!!!");
+// }
 
-var userValueInput = prompt("Please enter your temperature you wish to convert");
-userValueInput = parseInt(userValueInput);
-var userTypeInput = prompt("What degree is this?");
-
-const celciusToFarenheit = (userValueInput / 5) * (9 + 32);
-const farenheitToCelcius = (userValueInput - 32) * (5 / 9);
-if (Number.isInteger(userValueInput) && userTypeInput.startsWith('f')){
-     console.log("Here is your conversion: " + farenheitToCelcius + "C");
-} else if (Number.isInteger(userValueInput) && userTypeInput.startsWith('c')){
-     console.log("Here is your conversion: " + celciusToFarenheit + "F" );
-} else {
-     console.log("Invalid entry please try again!!!");
+function tempConvert(){
+     let degreeValue = document.querySelector(".valueInput").value;
+     let inputType = document.querySelector("#inputSelector").value;
+     let outputType = document.querySelector("#outputSelector").value;
+     let valueOutput = doucument.querySelector("#conversionOutput");
+     //alert(degreeValue + " Converting this value...");
+     //alert(inputType);
+     //alert(outputType);
+     const celsiusToFahrenheit = (degreeValue / 5) * (9 + 32);
+     const fahrenheitToCelsius = (degreeValue - 32) * (5 / 9);
+     const fahrenheitToKelvin = (degreeValue -32) * (5/9) + 273.15;
+     const celsiusToKelvin = (degreeValue + 273.15);
+     const kelvinToFahrenheit = (degreeValue - 273.15) * (9/5) + 32;
+     const kelvinToCelsius = (degreeValue - 273.15);
+     if(inputType != outputType){
+          if(inputType === "Fahrenheit" && outputType === "Celsius"){
+               //alert(fahrenheitToCelsius + " C");
+               doucument.querySelector("#conversionOutput").innerHTML = fahrenheitToCelsius + " C";
+               //return fahrenheitToCelsius;
+          }
+          else if (inputType === "Fahrenheit" && outputType === "Kelvin"){
+               alert(fahrenheitToKelvin + " K");
+               return fahrenheitToKelvin;
+          }
+          else if (inputType === "Celsius" && outputType === "Fahrenheit"){
+               alert(fahrenheitToFahrenheit + " F");
+               return celsiusToFahrenheit;
+          }
+          else if (inputType === "Celsius" && outputType === "Kelvin"){
+               alert(fahrenheitToKelvin + " K");
+               return celsiusToKelvin;
+          }
+          else if (inputType === "Kelvin" && outputType === "Fahrenheit"){
+               alert(fahrenheitToFahrenheit + " F");
+               return kelvinToFahrenheit;
+          }
+          else {
+               alert(kelvinToCelsius + " C");
+               return kelvinToCelsius;
+          }
+     } else{
+          alert(degreeValue);
+          return degreeValue;
+     }
 }
+document.addEventListener("DOMContentLoaded", () =>{
+     const button = document.getElementById("convertButton");
+     button.addEventListener("click", (event) =>{
+          tempConvert();
+     });
+});
