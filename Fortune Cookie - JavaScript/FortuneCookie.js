29ffdb34e1c7f28c982fc96824a205e95faaeae0
Shane Craig <shanecraig99@live.com>
2020-11-21 23:28:32 -0500

    JavaScript Updated
    Updated the value of  random value that will be shoved into the switch value.

--- Fortune Cookie - JavaScript/FortuneCookie.js
+++ Fortune Cookie - JavaScript/FortuneCookie.js
@@ -2,36 +2,33 @@ function fortuneGenerator(){
     let fortuneCookieValue = 6;
     const fortuneValue = document.getElementById("fortuneOutput");
     const userName = document.getElementById("userInput").value;
-    let randomValue = Math.random() * fortuneCookieValue;
+    let randomValue = Math.floor(Math.random() * fortuneCookieValue);
     switch(randomValue){
         case 0:
-            document.getElementById("fortuneOutput").innerHTML= userName + ", " + "It is uncertain....";
-            console.log("inside case zero");
-             break;
+            fortuneValue.innerText = userName + ", " + "It is uncertain....";
+            break;
         case 1:
-            fortuneValue.innerHTML = userName + ", " + "You will be very rich..";
+            fortuneValue.innerText = userName + ", " + "You will be very rich..";
             break;
         case 2:
-            fortuneValue.innerHTML = userName + ", " + "You will live a life of unfortuante events..";
+            fortuneValue.innerText = userName + ", " + "You will live a life of unfortuante events..";
             break;
         case 3:
-            fortuneValue.innerHTML = userName + ", " +"You will live a life of happiness...";
+            fortuneValue.innerText = userName + ", " +"You will live a life of happiness...";
             break;
         case 4:
-            fortuneValue.innerHTML = userName + ", " + "A special person will be visiting soon with great news..";
+            fortuneValue.innerText = userName + ", " + "A special person will be visiting soon with great news..";
             break;
         case 5:
-            fortuneValue.innerHTML = userName + ", " + "You will be finding yourself with a great amount of success...";
+            fortuneValue.innerText = userName + ", " + "You will be finding yourself with a great amount of success...";
             break;
-        default:
-             console.log("an error has occured");
-             break;
-        }
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () =>{
     const cookieButton = document.getElementById('cookieButton');
     cookieButton.addEventListener('click', (event) =>{
+        event.preventDefault();
         fortuneGenerator();
     });
 });
